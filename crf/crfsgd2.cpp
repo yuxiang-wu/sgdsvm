b13364968bcdfd84ff915fbdcacf34ef130d679a
cvs <cvs>
2009-10-31 11:17:48 +0000

    added graphs versus time. removed code to generate measurements every epoch before first cepoch. removed crfsgdqn data from log (was old version)

--- crf/crfsgd2.cpp
+++ crf/crfsgd2.cpp
@@ -1859,7 +1859,7 @@ main(int argc, char **argv)
       while (crf.getEpoch() < epochs)
         {
           tm.start();
-          int ce = (crf.getEpoch() < cepochs) ? 1 : cepochs;
+          int ce = cepochs; // (crf.getEpoch() < cepochs) ? 1 : cepochs;
           crf.train(train, ce, &tm);
           tm.stop();
           if (verbose)
